daec5603684a76f4c7221d084cfead18aaf134f9
edySnake <contactoedysnake@gmail.com>
2023-06-14 21:36:34 -0600

    Inicio de proyecto, prueba Aerolinea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // Importa el módulo HttpClientModule
 
 import { AppComponent } from './app.component';
+import { AirComponent } from './air/air.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AirComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule // Agrega el módulo HttpClientModule a la lista de imports
   ],
   providers: [],
   bootstrap: [AppComponent]
